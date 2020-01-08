b04a902b0138650343f529746bff92f4bde532e0
lzh <1062176745@qq.com>
2020-01-08 23:53:51 +0800

    bee~

---
+++ src/num_20.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class num_20 {
+    public boolean isValid(String s) {
+        char symbol[]=s.toCharArray();
+        Stack<Character> stack=new Stack<>();
+        char ch1,ch2;
+        if(symbol.length!=0){
+            for(char chars:symbol){
+                if(stack.size()==0){
+                    stack.push(chars);
+                }else if(isSym(stack.peek(),chars)){
+                    stack.pop();
+                }else {
+                    stack.push(chars);
+                }
+            }
+        }
+        return stack.size()==0;//thinking:the use of hashmap to make the searching process faster.
+    }//problem:occupation of internal storage is too high.
+    public boolean isSym(char ch1,char ch2){
+        return ((ch1=='{'&& ch2=='}')||(ch1=='['&& ch2==']')||(ch1=='('&& ch2==')'));
+    }
+}
+
+
