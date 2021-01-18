6651f9f33f1f5b89e24d34ce24929e34396f7cc7
Chenleely <52369993+Chenleely@users.noreply.github.com>
2021-01-18 16:33:22 +0800

    leetcode_1

--- src/Answer/Test.java
+++ src/Answer/Test.java
@@ -2,8 +2,8 @@ package answer;
 
 public class Test {
     public static void main(String[] args) {
-        num_53 test=new num_53();
-        int[] num={-3,-2,0,-1};
-        System.out.println(test.maxSubArray(num));
+        num_1 test=new num_1();
+        int[] result=test.twoSum1(new int[]{3, 2, 4},6);
+        System.out.println(result);
     }
 }
--- src/Answer/num_18.java
+++
@@ -1,9 +0,0 @@
-package answer;
-
-import java.util.List;
-//四数之和
-public class num_18 {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-
-    }
-}
