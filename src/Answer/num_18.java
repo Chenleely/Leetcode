b8e6013dae319b4dfe384e93bafdc3b1ffc11771
luozhihua <1062176745@qq.com>
2021-03-14 20:15:11 +0800

    test

--- src/Answer/num_18.java
+++ src/Answer/num_18.java
@@ -3,7 +3,5 @@ package answer;
 import java.util.List;
 //四数之和
 public class num_18 {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-
-    }
+    
 }
---
+++ src/Answer/num_2.java
@@ -0,0 +1,7 @@
+package answer;
+
+public class num_2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+    }   
+}
